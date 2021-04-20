15e906bf6fa02a462a72dfd42bfd77debbf0288d
Contributor Shephard <80837156+vgtu2021pn@users.noreply.github.com>
2021-04-20 18:34:04 +0300

    mysqli

--- includes/MySQLHandler.php
+++ includes/MySQLHandler.php
@@ -2,16 +2,15 @@
 require_once(dirname(dirname(__FILE__)) . '/LocalSettings.php');
 
 ini_set('display_errors',1); 
- error_reporting(E_ALL);
-$con = mysql_connect($host,$dbuser,$dbpass);
+error_reporting(E_ALL);
+$con = mysqli_connect($host,$dbuser,$dbpass);
 
-//mysql_select_db($dbname,$con);
-@mysql_select_db($dbname,$con) or die( "Unable to connect to the database: $dbname");
+@mysqli_select_db($con,$dbname) or die("Unable to connect to the database: $dbname");
 
 
 if (!$con)
  {
- die('Could not connect: ' . mysql_error());
+ die('Could not connect: ' . mysqli_error($con));
  }
 
 ?>
