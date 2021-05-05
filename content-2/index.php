8533c9131982caf05f50b2178017eb63353cc2d0
Contributor Shephard <80837156+vgtu2021pn@users.noreply.github.com>
2021-05-05 14:47:33 +0300

    Update index.php

--- content-2/index.php
+++ content-2/index.php
@@ -35,7 +35,7 @@
 <body>
 <div class="row">
 	<div class="four columns centered">
-		<br/><br/><a href="../index.php"><img src="../images/bricks.jpg" /></a>
+		<br/><br/><a href="../index.php"><img src="../images/bricks.jpg" alt="Main Bricks Page"/></a>
 		<p>
 			<fieldset>
 				<legend>Details</legend>
