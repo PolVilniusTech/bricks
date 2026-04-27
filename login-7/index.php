a796bb63fc3172c6c9ce661eafd16a255754d4d3
Contributor Shephard <80837156+vgtu2021pn@users.noreply.github.com>
2026-04-27 21:45:58 +0300

    Update index.php

--- login-7/index.php
+++ login-7/index.php
@@ -2,32 +2,50 @@
 	require_once(dirname(dirname(__FILE__)) . '/includes/MySQLHandler.php');
 	
 	session_start();
-	if (isset($_POST["username"])) { $_SESSION['username'] = $_POST["username"]; }
-	if (isset($_POST["passwd"])) { $_SESSION['passwd'] = $_POST["passwd"]; }
-	if(isset($_POST['submit']))  {
+	global $page;
+	$malformed=false;
+	if(isset($_POST['submit'])) {
 	$username=$_POST['username'];
 	$pwd=md5($_POST['passwd']);
 	$sql="SELECT * FROM users WHERE name='$username' and password='$pwd'";
+	try {
 	$result=mysqli_query($con,$sql);
-	$count=mysqli_num_rows($result);
-	if($count>0){
-		$_SESSION['valid'] = "1";
-	} 	} else {
-		$_SESSION['valid'] = "0";
-		if ($_GET["p"] !== "login") { header("Location: index.php?p=login"); }
+	} catch (Exception $e) {
+	$malformed=true;
 	}
-	global $page; $page = "index";
+	if(isset($result) && is_object($result)) {
+	$count=mysqli_num_rows($result);
+	} else {
+	$count=0;
+	}
+	if($count>0) {
+		$_SESSION['valid'] = "1";
+		header("Location: index.php?p=index");
+		exit();
+	}
+	$_SESSION['valid'] = "0";
+	header("Location: index.php?p=incorrent");
+	exit();
+	} else {
+		$page = "index";
+		if(!isset($_SESSION['valid'])) { $_SESSION['valid'] = "0"; $page = "login"; $pagetitle = "Log In"; }
+		if(isset($_SESSION['valid']) && $_SESSION['valid'] == "0") { $page = "login"; $pagetitle = "Log In"; }
+	}
+
 	if (isset($_GET["p"])) {
 		// redirect on invalid page attempts
 		if (!in_array(strtolower($_GET["p"]), array(
-			"index","login","logout"
+			"incorrect","index","login","logout"
 		))) {
+			session_destroy();
 			header("Location: index.php");
-			exit("Invalid parameter. <a href='index.php'>Continue</a>.");
+			exit();
 		}
-		$page = $_GET["p"];
-		if ($_GET["p"] == "login") {$pagetitle = "Log In"; }
-	if ($_GET["p"] == "logout") {$pagetitle = "Log Out"; $_SESSION['valid'] = "0"; session_destroy(); }
+
+		if ($_GET["p"] == "incorrect") { $page = "incorrect"; $pagetitle = "Error"; }
+		if ($_GET["p"] == "index") { $page = "index"; $pagetitle = "Welcome"; }
+		if ($_GET["p"] == "login") { $page = "login"; $pagetitle = "Log In"; $_SESSION['valid'] = "0"; }
+		if ($_GET["p"] == "logout") { $page = "logout"; $pagetitle = "Log Out"; session_destroy(); }
 	}
 	
 ?><!DOCTYPE html>
@@ -36,9 +54,9 @@
 <!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width" />
-  <title>Bricks Login Form #6</title>  
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Bricks Login Form #7</title> 
   <!-- Included CSS Files (Uncompressed) -->
   <!--
   <link rel="stylesheet" href="../stylesheets/foundation.css">
@@ -49,35 +67,39 @@
   <script src="../javascripts/modernizr.foundation.js"></script>
   <!-- IE Fix for HTML5 Tags -->
   <!--[if lt IE 9]>
-    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
   <![endif]-->
-  <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 </head>
 <body>
 	<div class="row">
 		<div class="four columns centered">
-			<br/><br/><a href="../index.php"><img src="../images/bricks.jpg" alt="Main Bricks Page"/></a><br/>		
+			<br/><br/><a href="../index.php"><img src="../images/bricks.jpg" alt="Main Bricks Page"/>&nbsp;</a><br/>		
 			<?php
 				if ($page == "login") { ?><br/>
 					<form method="post" action="<?php echo $_SERVER["SCRIPT_NAME"]; ?>">
 						<fieldset>
 						<legend>Login</legend>
 							<p><label for="username">Username:</label>
-							<input type="text" name="username" id="username" 
-							class="textinput" /></p>
-							<p><label for="password">Password:</label><input 
-							type="password" name="passwd" id="passwd" 
-							class="textinput" /></p>
-							<p><input type="submit" name="submit" class="button" value="Enter" /></p>
+							<input type="text" name="username" id="username" class="textinput"></p>
+							<p><label for="password">Password:</label>
+							<input type="password" name="passwd" id="passwd" class="textinput"></p>
+							<p><input type="submit" name="submit" class="button" value="Enter"></p>
 						</fieldset>
 					</form>
-			<?php };
+			<?php }
 			if ($page == "index") { ?><br/>
-				<p>You are succesfully logged in. | <a  class="small button" href="index.php?p=logout">Log Out</a></p>		
-			<?php };
+				<p>You are succesfully logged in the Team Account. | <a class="small button" href="index.php?p=logout">Log Out</a></p>		
+			<?php }
+			if ($page == "incorrect") { ?><br/>
+				<p>You input wrong Username and Password. | <a class="small button" href="index.php?p=login">Try Again</a></p>		
+			<?php }
+			if ($malformed) { ?><br/>
+				<p>Malformed SQL.</p>		
+			<?php }
 			if ($page == "logout") { ?><br/>
-				<p>You have successfully been logged out and will be redirected shortly to the login page.</p>
-			<?php }; ?>
+				<p>You have successfully been logged out from the Team Account. | <a class="small button" href="index.php?p=login">Log In</a></p>
+			<?php } ?>
 		</div>
 	</div>
   <!-- Included JS Files (Uncompressed) -->
@@ -106,4 +128,3 @@
   <script src="../javascripts/app.js"></script>  
 </body>
 </html>
-
