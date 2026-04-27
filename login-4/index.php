5499cfbdb58ccf97b3967b9c054d1eb96a8511b1
Contributor Shephard <80837156+vgtu2021pn@users.noreply.github.com>
2026-04-27 19:13:33 +0300

    Update index.php

--- login-4/index.php
+++ login-4/index.php
@@ -1,18 +1,28 @@
 <?php
 	require_once(dirname(dirname(__FILE__)) . '/includes/MySQLHandler.php');	
 	$sSuccessMsg = "<div class=\"alert-box\">  You are not logged in.  <a href=\"\" class=\"close\">&times;</a></div>";
+	$malformed=false;
 	if(isset($_POST['submit'])) {
-		$username=$_POST['username'];
-		$pwd=$_POST['passwd'];
-		$username = '"' . $username . '"';
-		$pwd = '"' . $pwd . '"';
-		$sql="SELECT * FROM users WHERE name=($username) and password=($pwd)";
-		$result=mysqli_query($con,$sql);
-		$count=mysqli_num_rows($result);
+	$username=$_POST['username'];
+	$pwd=$_POST['passwd'];
+	$username = '"' . $username . '"';
+	$pwd = '"' . $pwd . '"';
+	$sql="SELECT * FROM users WHERE name=($username) and password=($pwd)";
+	try {
+	$result=mysqli_query($con,$sql);
+	} catch (Exception $e) {
+	$malformed=true;
+	}
+	if(isset($result) && is_object($result)) {
+	$count=mysqli_num_rows($result);
+	} else {
+	$count=0;
+	}
 		$sSuccessMsg = ($count>0?
 		"<div class=\"alert-box success\">Succesfully logged in.<a href=\"\" class=\"close\">&times;</a></div>":
 		"<div class=\"alert-box alert\">Wrong user name or password.<a href=\"\" class=\"close\">&times;</a></div>");
-								}
+		if($malformed) { $sSuccessMsg = $sSuccessMsg . "<div class=\"alert-box alert\">Malformed SQL.<a href=\"\" class=\"close\">&times;</a></div>"; }
+	}
 ?><!DOCTYPE html>
 <!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
 <!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
@@ -36,18 +46,18 @@
 <body>
 	<div class="row">
 		<div class="four columns centered">
-			<br/><br/><a href="../index.php"><img src="../images/bricks.jpg" alt="Main Bricks Page"/></a><br/><br/>
+			<br/><br/><a href="../index.php"><img src="../images/bricks.jpg" alt="Main Bricks Page"/>&nbsp;</a><br/><br/>
 			<form method="POST" action="index.php" enctype="application/x-www-form-urlencoded">
 				<fieldset>
 				<legend>Login</legend>
-				<p><?php echo $sSuccessMsg;?></p>
-				<p>Username: <input type="text" name="username" id="username" size="25" required/></p>
-				<p>Password: <input type="password" name="passwd" id="passwd" size="25" required/></p>
-				<p><input type="submit" class="small button" name="submit" id="submit" value="Submit"/></p>
+				<p><?php echo $sSuccessMsg;?>&nbsp;</p>
+				<p>Username: <input type="text" name="username" id="username" size="25" required></p>
+				<p>Password: <input type="password" name="passwd" id="passwd" size="25" required></p>
+				<p><input type="submit" class="small button" name="submit" id="submit" value="Submit"></p>
 				</fieldset>
 			</form>
 		</div><br/><br/><br/>
-	<center>
+	<p style='align-left:auto; align-right:auto;'>
 		<?php 
 			if($showhint === true && isset($sql)) { 
 				echo '<div class="eight columns centered"><div class="alert-box secondary">SQL Query: ';
@@ -55,42 +65,27 @@
 				echo '<a href="" class="close">&times;</a></div></div>';			
 									} 
 		?>
-	</center>
+	</p>
 </div>
   
   <!-- Included JS Files (Uncompressed) -->
   <!--
   
   <script src="../javascripts/jquery.js"></script>
-  
   <script src="../javascripts/jquery.foundation.mediaQueryToggle.js"></script>
-  
   <script src="../javascripts/jquery.foundation.forms.js"></script>
-  
   <script src="../javascripts/jquery.foundation.reveal.js"></script>
-  
   <script src="../javascripts/jquery.foundation.orbit.js"></script>
-  
   <script src="../javascripts/jquery.foundation.navigation.js"></script>
-  
   <script src="../javascripts/jquery.foundation.buttons.js"></script>
-  
   <script src="../javascripts/jquery.foundation.tabs.js"></script>
-  
   <script src="../javascripts/jquery.foundation.tooltips.js"></script>
-  
   <script src="../javascripts/jquery.foundation.accordion.js"></script>
-  
   <script src="../javascripts/jquery.placeholder.js"></script>
-  
   <script src="../javascripts/jquery.foundation.alerts.js"></script>
-  
   <script src="../javascripts/jquery.foundation.topbar.js"></script>
-  
   <script src="../javascripts/jquery.foundation.joyride.js"></script>
-  
   <script src="../javascripts/jquery.foundation.clearing.js"></script>
-  
   <script src="../javascripts/jquery.foundation.magellan.js"></script>
   
   -->
